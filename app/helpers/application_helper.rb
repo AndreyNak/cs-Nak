7c9e674f9b99242d4c06e5a8d81fc9303cc6b08b
Andrey Nakaryakov <andrey.nakaryakov@symmetRE.com>
2023-01-13 20:07:52 +0300

    Added sidebar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@
 
 module ApplicationHelper
   def load_svg(path)
-    File.open("app/assets/images/#{path}", 'rb') do |file|
+    File.open("app/assets/images/svg/#{path}", 'rb') do |file|
       raw(file.read)
     end
   end
