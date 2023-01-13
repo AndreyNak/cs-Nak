eb5799f0ec316c3b72d2ca7778b655466f5e9050
Andrey Nakaryakov <andrey.nakaryakov@symmetRE.com>
2023-01-13 15:16:08 +0300

    Added load_svg helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def load_svg(path)
+    File.open("app/assets/images/#{path}", 'rb') do |file|
+      raw(file.read)
+    end
+  end
 end
